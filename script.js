9c70c2638b53d23d71bf72719ef92fcbf5f32b8f
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-13 15:32:10 +0200

    Create box via js, style with css and flex row .row

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const row = document.createElement('row');
 row.classList.add('row');
 
 container.appendChild(row);
+
+const box = document.createElement('box');
+
+box.classList.add('box');
+
+row.appendChild(box);
