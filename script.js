5328df7d7d923b95c04c39fe6946fb2b7842b6c2
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-13 14:43:36 +0200

    Remove dot from row in add argument for classList

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const container = document.querySelector('.container');
 
 const row = document.createElement('row');
 
-row.classList.add('.row');
+row.classList.add('row');
 
 container.appendChild(row);
