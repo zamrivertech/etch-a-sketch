3a906af10afea962b7e54bcb09e331627b06fd9f
zamrivertech <mohamadtasul8000@gmail.com>
2026-05-14 13:27:26 +0200

    Clean squares everytime a new number of sqaures is received

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const container = document.querySelector('.container');
 const getSquares = document.querySelector('#squaresNum'); //64;// prompt('Number of squares?', 42);
 
 getSquares.addEventListener('click', () => {
-    
+
+while (container.firstChild) {//this erases the whole container
+    //please consolelog this code to understand how it works
+    container.removeChild(container.firstChild);
+}
+
 let squares = prompt('Set number of squares?', 32);
 
 if (squares > 100) {
@@ -12,9 +17,9 @@ if (squares > 100) {
 
 let drawable = false;
 
-for (let i = 0; i < squares; i++) {
+for (let i = 0; i < squares; i++) {   
 
-const row = document.createElement('row');
+let row = document.createElement('row');
 
 row.classList.add('row');
 
@@ -22,7 +27,7 @@ container.appendChild(row);
 
     for (let j = 0; j < squares; j++) {
 
-        const box = document.createElement('box');
+        let box = document.createElement('box');
 
         box.classList.add('box');
 
